0618380d728203573f9627c07259c7d02ed49d18
shahab <shahab@chla.usc.edu>
2017-07-24 14:13:05 -0700

    Update esBoxplot.R

--- r/esBoxplot.R
+++ r/esBoxplot.R
@@ -137,7 +137,7 @@ function(es, features = '', covar='', annotation = annot, colPallette = colorPal
     
     p = ggplot(aes_string(x=variable, y=value, fill=covar), data=gges) + 
       geom_boxplot(
-               position = position_dodge(width = .1), 
+               position = position_dodge(width = .4), 
                width = 0.3) + 
       xlab("Genes") + 
       ylab("Expression Value") +
@@ -164,7 +164,7 @@ function(es, features = '', covar='', annotation = annot, colPallette = colorPal
       theme_set(theme_grey())
       p = ggplot(aes_string(x=variable, y=value), data=gges) +     
         geom_boxplot(
-               position = position_dodge(width = .1), 
+               position = position_dodge(width = .4), 
                width = 0.3) + 
         xlab("Genes") + 
         ylab("Expression Value") +
@@ -178,7 +178,7 @@ function(es, features = '', covar='', annotation = annot, colPallette = colorPal
     {
       
       p = ggplot(aes_string(x=variable, y=value), data=gges) +     
-        geom_boxplot(aes_string(fill=covar), position=position_dodge(width=.1), width=0.3) + 
+        geom_boxplot(aes_string(fill=covar), position=position_dodge(width=.4), width=0.3) + 
         xlab("Genes") + 
         #ylim(0, max(gges$value))+
         theme(axis.title.x = element_text(face="bold",  vjust=-.6, size=20), 
